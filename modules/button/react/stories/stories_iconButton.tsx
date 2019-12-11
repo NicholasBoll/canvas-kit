adae589be5f9d47e1dcc179249ea8462e076a6bd
Nicholas Boll <nicholas.boll@workday.com>
2019-12-11 16:09:05 -0700

    feat: Add ability to force psuedo classes for visual regression

--- modules/button/react/stories/stories_iconButton.tsx
+++ modules/button/react/stories/stories_iconButton.tsx
@@ -23,11 +23,10 @@ import {
 import README from '../README.md';
 import {IconButtonProps} from '../lib/IconButton';
 
-const blueBackground: CSSObject = {
+const iconButtonLayout: CSSObject = {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'center',
-  backgroundColor: '#0875e1',
   margin: '0 10px',
   padding: '24px',
   maxWidth: 'max-content',
@@ -35,6 +34,11 @@ const blueBackground: CSSObject = {
   button: {
     margin: '0 12px',
   },
+};
+
+const blueBackground: CSSObject = {
+  ...iconButtonLayout,
+  backgroundColor: '#0875e1',
 };
 
 const commonIconButtonProps: Pick<IconButtonProps, 'aria-label' | 'title' | 'icon'> = {
@@ -340,7 +344,151 @@ storiesOf('Components|Buttons/Button/React/Icon Button', module)
         <ToggleIconButtonWrapper variant={IconButton.Variant.InverseFilled} />
       </div>
     </div>
-  ));
+  ))
+  .add('States', () => {
+    const states = ['Default', 'hover', 'focus', 'active', 'focus active', 'focus hover'];
+    const variants = [
+      'Inverse',
+      'InverseFilled',
+      'Plain',
+      'Circle',
+      'CircleFilled',
+      'Square',
+      'SquareFilled',
+    ] as const;
+
+    return (
+      <div data-whatinput="keyboard">
+        <h3>Not toggleable</h3>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              {states.map(state => (
+                <th key={state}>{state}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {variants.map(variant => (
+              <tr key={variant}>
+                <th>{variant}</th>
+                {states.map(state => (
+                  <td key={state}>
+                    <div css={variant.includes('Inverse') ? blueBackground : iconButtonLayout}>
+                      <IconButton
+                        className={state}
+                        variant={IconButton.Variant[variant]}
+                        aria-label="test"
+                        icon={activityStreamIcon}
+                        shouldChangeStateSeparator={true}
+                      ></IconButton>
+                    </div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <hr />
+        <h3>toggled: true</h3>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              {states.map(state => (
+                <th key={state}>{state}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {variants.map(variant => (
+              <tr key={variant}>
+                <th>{variant}</th>
+                {states.map(state => (
+                  <td key={state}>
+                    <div css={variant.includes('Inverse') ? blueBackground : iconButtonLayout}>
+                      <IconButton
+                        className={state}
+                        variant={IconButton.Variant[variant]}
+                        aria-label="test"
+                        icon={activityStreamIcon}
+                        shouldChangeStateSeparator={true}
+                        toggled={true}
+                      ></IconButton>
+                    </div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <hr />
+        <h3>toggled: false</h3>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              {states.map(state => (
+                <th key={state}>{state}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {variants.map(variant => (
+              <tr key={variant}>
+                <th>{variant}</th>
+                {states.map(state => (
+                  <td key={state}>
+                    <div css={variant.includes('Inverse') ? blueBackground : iconButtonLayout}>
+                      <IconButton
+                        className={state}
+                        variant={IconButton.Variant[variant]}
+                        aria-label="test"
+                        icon={activityStreamIcon}
+                        shouldChangeStateSeparator={true}
+                        toggled={false}
+                      ></IconButton>
+                    </div>
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <hr />
+        <h3>disabled</h3>
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Default</th>
+            </tr>
+          </thead>
+          <tbody>
+            {variants.map(variant => (
+              <tr key={variant}>
+                <th>{variant}</th>
+
+                <td>
+                  <div css={variant.includes('Inverse') ? blueBackground : iconButtonLayout}>
+                    <IconButton
+                      disabled
+                      className=""
+                      variant={IconButton.Variant[variant]}
+                      aria-label="test"
+                      icon={activityStreamIcon}
+                      shouldChangeStateSeparator={true}
+                    ></IconButton>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  });
 
 storiesOf('Components|Buttons/Button/React/Icon Button Toggle Group', module)
   .addParameters({component: IconButtonToggleGroup})
